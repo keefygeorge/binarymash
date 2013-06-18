03c0d835ab03471fd4c5dd46476e630e4067439b
doodo477 <doodo477@gmail.com>
2013-06-18 22:38:17 +0930

    Updated the index and the server. powering ahead with mongoose and web sockets!

--- static_server/server.js
+++ static_server/server.js
@@ -4,21 +4,20 @@ connect.createServer(
 ).listen(8080);
 console.log("Web server loaded on socket 8080");
 
-
 // ################################
 // Mongoose
 // Grab the mongoose
 var mongoose = require('mongoose');
 
 // Not really required...
-var TodoSchema = mongoose.Schema({desc:String});
+var TodoSchema = mongoose.Schema({message:String});
 
 // Create a model, using the TodoSchema, the collection is 
 // Derived off the model name. We name it anyway to be TodoModel
-var TodoModel = mongoose.model('todo',TodoSchema);
+var TodoModel = mongoose.model('message',TodoSchema);
 
 // Create the connection...
-var db = mongoose.createConnection('mongodb://localhost/mydb');
+var db = mongoose.connect('mongodb://localhost/mydb').connection;
 
 // If there is any error, just log it out and set the db connection to 
 // be undefined. Nasty I know
@@ -27,8 +26,9 @@ db.on('error', function(err){
 	console.log("failed to connect:"+ err);
 	db = undefind;
 });
-// Normal situation open the connection not really required here.
+
 /*
+// Normal situation open the connection not really required here.
 db.once('open', function(){
 	// create the model
 	var UserModel = db.model('users', { name:String , ha:String });
@@ -48,11 +48,30 @@ db.once('open', function(){
 // ################################
 
 // Use npm install ws
-var WebSocketServer = require('ws').Server
+var WebSocketServer = require('ws').Server;
 var ws = new WebSocketServer({port: 8081});
 ws.on('connection', function(ws) {
 	ws.on('message', function(message) {
 		console.log(message);
-		console.log('received: %s', message);
+
+		// parse the string into a JLO
+		var obj = JSON.parse(message);
+		if (obj.type=="getAll"){
+			TodoModel.find(function(err,items){
+				// error occured dont do anything
+				if (err) return;
+
+				var tmp = {
+					type:"setAll",
+					data:items
+				}
+
+				var msgStr = JSON.stringify(tmp);
+				console.log("Sending:" + msgStr);
+				ws.send(msgStr);				
+			});
+		}
+
+		console.log('received:' + JSON.parse(message));
 	});
 });
